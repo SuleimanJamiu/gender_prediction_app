1a9cd7add3edeaeea76727a210fe54e35ab06f16
SuleimanJamiu <solomonola1k@gmail.com>
2026-04-16 18:30:27 +0100

    initial deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = require("./router/genderizeRoute")
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/api', router);
+
+
+app.listen(PORT, () => {  console.log(`Server is running on port ${PORT}`);
+});
